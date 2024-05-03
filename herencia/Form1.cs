a45dc23cdd82ff25b910713d3563c4349b44d982
LuisPabloLeon <163448689+LuisPabloLeon@users.noreply.github.com>
2024-05-03 13:53:52 -0600

    Add files via upload

---
+++ herencia/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void suma_Click(object sender, EventArgs e)
+        {
+            //voy a darle comportamiento a mi boton voy a utilizar mi clase hija sumar
+            Sumar suma = new Sumar();//instancie, o hice un nuevo objeto suma llamar al metodo clase sumar operar
+            textBox3.Text = suma.operar (float.Parse(textBox1.Text),float.Parse(textBox2.Text)).ToString();
+        }
+
+        private void resta_Click(object sender, EventArgs e)
+        {
+            {
+                Restar restar = new Restar();
+                textBox3.Text = restar.operar(float.Parse(textBox1.Text), float.Parse(textBox2.Text)).ToString();
+            }
+        }
+
+        private void Mulriplicar_Click(object sender, EventArgs e)
+        {
+            {
+                Multiplicar multiplica = new Multiplicar();
+                textBox3.Text = multiplica.operar(float.Parse(textBox1.Text), float.Parse(textBox2.Text)).ToString();
+            }
+        }
+
+        private void Limpa_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dividir_Click(object sender, EventArgs e)
+        {
+            {
+                Dividir divide = new Dividir();
+                textBox3.Text = divide.operar(float.Parse(textBox1.Text), float.Parse(textBox2.Text)).ToString();
+            }
+        }
+    }
+}
---
+++ herencia/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herencia
+{
+    class operacion
+    {
+        //declaracion de variables que usa la clase
+        //tipo de acceso        tipo de dato          nombre de dato
+        protected               float                     valor1;
+        protected               float                      valor2;
+        protected               float                     resultado;
+
+        //definicion de atributos: sin atributos
+        //metodos o funciondes de operaciones
+        /*los meodos que se ponen a continuacion
+         * son las funciones de obtener e valor
+         * de la caja de texto y guardarlo en la 
+         * memoria dentro de la variable indicada*/
+        //USAMOS UN contructor POR CADA CAJA DE MEMORIA
+        public float Valor1//CAJA DE MEMORIA 1
+        {
+            get { return valor1; }//Vamos por el valor
+                                  //de la caja de texto
+            set { valor1 = value; }//lo guardamos en la memoria
+        }
+        public float Valor2//Caja de memoria 2
+        {
+            get { return valor2; }
+            set { valor2 = value; }
+        }
+        public float Resultado//Caja de memoria 3
+        {
+            get { return resultado; }
+            set { resultado = value; }
+        }
+    }
+    //creamos nuestras clases hijo que usara los
+    //metodos de crear dato
+    class Sumar : operacion //los : nos dan permiso de usar
+                            //lo que tiene operacion
+    {
+        /*las clases hijas tambien pueden tener sus
+         * propios atributos y metodos
+         * pueden tener variables de uso solamente
+         * para la clase hijo
+         * declarar variables de esta clase
+         * atributos de esta clase
+         * metodos de clase*/
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1;
+            valor2 = v2;
+            return resultado = valor1 + valor2;
+        }
+    }
+    class Restar : operacion
+    {
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1;
+            valor2 = v2;
+            return resultado = valor1 - valor2;
+        }
+    }
+    class Multiplicar : operacion
+    {
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1;
+            valor2 = v2;
+            return resultado = valor1 * valor2;
+        }
+    }
+    class Dividir : operacion
+    {
+        public float operar(float v1, float v2)
+        {
+            valor1 = v1;
+            valor2 = v2;
+            if (valor1 == 0)//validacion de que el numero no sea 0
+            {
+                MessageBox.Show("No puedes dividir entre 0");
+            }
+            return resultado = valor1 / valor2;
+        }
+    }
+    //esto es un comentario nuevo 26 de abril
+    //esto es un segunod comentario nuevo 26 de abril
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
